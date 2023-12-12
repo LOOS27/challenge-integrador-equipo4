5f6d18817750c761f44d07731623f1e4089a2ae7
sohe89 <soniaherreradg@gmail.com>
2023-12-11 21:23:44 -0300

    se instala servidor express, nodemon.

---
+++ app.js
@@ -0,0 +1,12 @@
+//Llamo al módulo de Express
+const express = require('express');
+
+// Creo una variable para llamar a variable Express
+const app = express();
+
+//Declaro variable del puerto.
+const PORT = 3004;
+
+app.use(express.static('public'));
+
+app.listen(PORT , () => console.log(`servidor corriendo en http://localhost${PORT}`));
