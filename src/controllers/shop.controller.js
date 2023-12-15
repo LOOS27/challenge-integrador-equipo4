26b9b2115aa656865e1493cc6f626b1bc89e239a
sohe89 <soniaherreradg@gmail.com>
2023-12-15 12:28:53 -0300

    Actualizacion 15/12/2023

--- src/controllers/shop.controller.js
+++ src/controllers/shop.controller.js
@@ -1,13 +1,36 @@
 const path = require ('path');
+
+const data  = [
+    {
+    product_id: 1,
+    licence_name: "Pokemon",
+    category_name: "Figuras coleccionables",
+    product_name: "Pidgeotto",
+    product_description: "Figura coleccionable pokemon",
+    product_price: 1799.99,
+    dues: 10,
+    product_sku: "PKM001001",
+    img_front: "/img/multimedia/pokemon/pidgeotto-1.webp",
+    img_back: "/img/multimedia/pokemon/pidgeotto-box.webp"
+    }
+    ]
 module.exports = {
     shop: (req , res) => {
+            
         res.render (path.resolve(__dirname, '../views/shop/shop.ejs'), {
             title: "Shop",
+            data
         });
     },
     item: (req,res) => {
+        // Guarda en variable el id que viene de la request
+        const itemId = req.params.id;
+        //Filtra dentro de la variable data para encontra el product id que coincida con ItemId (request param)
+        const item = data.find(item => item.product_id == itemId);
+
         res.render (path.resolve(__dirname, '../views/shop/item.ejs'), {
             title: "Producto",
+            item
         });
     },
     addItem: (req,res) => res.send('Esta es la ruta para agregar un nuevo item'),
